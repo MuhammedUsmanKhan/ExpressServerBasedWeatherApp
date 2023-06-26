8ec5d03e0e5ae43135f28b29f20d7c7dbca92945
MuhammedUsmanKhan <uktech0310@gmail.com>
2023-06-26 17:04:24 +0500

    WeatherAppUsingExpressJSDone

--- server.mjs
+++ server.mjs
@@ -13,67 +13,455 @@ app.get('/currentWeather/:cityName', (req, res) => {
       min_temp: 33,
       humidity: 78,
       feels_like: 42,
-      pressure: 45
+      pressure: 45,
+      country: "Pakistan"
     },
     quetta: {
       date: "June 23,2023",
-      temp: 36,
+      temp: 22,
       max_temp: 38,
       min_temp: 33,
-      humidity: 78,
-      feels_like: 42,
-      pressure: 45
+      humidity: 40,
+      feels_like: 32,
+      pressure: 60,
+      country: "Pakistan"
     },
     london: {
       date: "June 23,2023",
-      temp: 36,
+      temp: 16,
       max_temp: 38,
       min_temp: 33,
-      humidity: 78,
-      feels_like: 42,
-      pressure: 45
+      humidity: 20,
+      feels_like: 22,
+      pressure: 33,
+      country: "England"
     },
     manchestor: {
       date: "June 23,2023",
-      temp: 36,
+      temp: 20,
       max_temp: 38,
       min_temp: 33,
-      humidity: 78,
-      feels_like: 42,
-      pressure: 45
+      humidity: 40,
+      feels_like: 44,
+      pressure: 64,
+      country: "England"
     },
     newjersey: {
       date: "June 23,2023",
-      temp: 36,
+      temp: 44,
       max_temp: 38,
       min_temp: 33,
-      humidity: 78,
-      feels_like: 42,
-      pressure: 45
+      humidity: 56,
+      feels_like: 48,
+      pressure: 50,
+      country: "America"
     },
     newyork: {
       date: "June 23,2023",
-      temp: 36,
+      temp: 45,
       max_temp: 38,
       min_temp: 33,
-      humidity: 78,
-      feels_like: 42,
-      pressure: 45
+      humidity: 88,
+      feels_like: 48,
+      pressure: 56,
+      country: "America"
     }
   }
 
-let userEnteredCityNam = req.params.cityName.toLowerCase()
-let requestedWeatherData = currWeatherData[userEnteredCityNam]
-if(requestedWeatherData){
-  res.send(requestedWeatherData)
-}else{
-res.status(404).send(`Weather data is not available for ${req.params.cityName}`)
+  let userEnteredCityNam = req.params.cityName.toLowerCase()
+  let requestedWeatherData = currWeatherData[userEnteredCityNam]
+  if (requestedWeatherData) {
+    res.send(requestedWeatherData)
+  } else {
+    res.status(404).send(`The Data of City (${req.params.cityName}) you are trying to access right now is not available in our Pre-Defined DataSet . So Kindly enter from the following Cities that are available in our DataSet :\n-Karachi\n-Quetta\n-NewYork\n-NewJersey\n-London\n-Manchestor  `)
+  }
+
+})
+app.get('/weatherForecast/:cityName', (req, res) => {
+  let WeatherForecastData = {
+    karachi: [
+      {
+        date: "June 25,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 46,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 43,
+        feels_like: 55,
+        pressure:33,
+        country: "Pakistan"
+      },
+      {
+        date: "June 27,2023",
+        temp: 38,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 53,
+        feels_like: 42,
+        pressure: 66,
+        country: "Pakistan"
+      },
+      {
+        date: "June 28,2023",
+        temp: 40,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 43,
+        feels_like: 48,
+        pressure: 66,
+        country: "Pakistan"
+      },
+      {
+        date: "June 29,2023",
+        temp: 32,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 40,
+        feels_like: 36,
+        pressure: 39,
+        country: "Pakistan"
+      },
+      {
+        date: "June 30,2023",
+        temp: 30,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 38,
+        feels_like: 44,
+        pressure: 32,
+        country: "Pakistan"
+      }
+    ],
+    london: [
+      {
+        date: "June 25,2023",
+        temp: 22,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 12,
+        feels_like: 24,
+        pressure: 66,
+        country: "England"
+      },
+      {
+        date: "June 26,2023",
+        temp: 12,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 22,
+        feels_like: 8,
+        pressure: 22,
+        country: "England"
+      },
+      {
+        date: "June 27,2023",
+        temp: 28,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 30,
+        feels_like: 26,
+        pressure: 23,
+        country: "England"
+      },
+      {
+        date: "June 28,2023",
+        temp: 10,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 0,
+        feels_like: 4,
+        pressure: 33,
+        country: "England"
+      },
+      {
+        date: "June 29,2023",
+        temp: 2,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: -2,
+        pressure: 22,
+        country: "England"
+      },
+      {
+        date: "June 26,2023",
+        temp: 16,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 20,
+        feels_like: 18,
+        pressure: 24,
+        country: "England"
+      }
+    ],
+    quetta: [
+      {
+        date: "June 25,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "England"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "England"
+      },
+      {
+        date: "June 27,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 28,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 29,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      }
+    ],
+    newyork: [
+      {
+        date: "June 25,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 27,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 28,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 29,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      }
+    ],
+    manchestor: [
+      {
+        date: "June 25,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 27,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 28,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 29,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      }
+    ],
+    newjersey: [
+      {
+        date: "June 25,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 27,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 28,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 29,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      },
+      {
+        date: "June 26,2023",
+        temp: 36,
+        max_temp: 38,
+        min_temp: 33,
+        humidity: 78,
+        feels_like: 42,
+        pressure: 45,
+        country: "Pakistan"
+      }
+    ],
+  }
+
+let userEnteredCityNam = req.params.cityName
+if(WeatherForecastData[userEnteredCityNam]){
+  res.send(WeatherForecastData[userEnteredCityNam])
+}
+else{
+  res.status(404).send(`Weather Forecast is not available for ${req.params.cityName}`)
 }
 
 })
-// app.get('/weatherForecast', (req, res) => {
-  
-// })
 app.use(express.static(path.join(__dirname, `staticWeatherApp`)))
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
--- staticWeatherApp/JSFile/app.mjs
+++ staticWeatherApp/JSFile/app.mjs
@@ -1,21 +1,191 @@
 let getWeatherBut = document.getElementById(`getWeather`)
+let getWeatherForecast = document.getElementById(`getWeatherForecast`)
+let weatherInp = document.getElementById(`weatherInp`)
+let forecastPart = document.getElementById(`forecastPart`)
+let forecastContainer = document.getElementById(`forecastData`)
 
-getWeatherBut.addEventListener(`click`,()=>{
+getWeatherBut.addEventListener(`click`, async (event) => {
 
+  event.preventDefault()
+  let userInput = document.getElementById(`weatherInp`).value
+  let countryNam = document.getElementById(`cityNam`)
+  let $Date = document.getElementById(`Date`)
+  let Temp = document.getElementById(`Temp`)
+  let Humidity = document.getElementById(`Humidity`)
+  let FeelsLike = document.getElementById(`FeelsLike`)
+  let Pressure = document.getElementById(`Pressure`)
+  //const openModal = document.getElementById(event.target.id);
+  const modal = document.getElementById('myModal');
+  const closeModal = modal.getElementsByClassName('modal-close')[0];
+  const modalContainer = modal.getElementsByClassName('modal-container')[0];
+  const modalOverlay = modal.getElementsByClassName('modal-overlay')[0]; 
+  // axios.get(`/currentWeather/${userInput}`)
+  //   .then((response) => {
+  //     // handle success
+  //     console.log(response.data);
+  // countryNam.innerText = `${currWeatherinput.data.country} / ${userInput}`
+  // $Date.innerText = currWeatherinput.data.date
+  // Temp.innerText = `${currWeatherinput.data.temp}°C`
+  // Humidity.innerText = `${currWeatherinput.data.humidity}%`
+  // FeelsLike.innerText = `${currWeatherinput.data.feels_like}°C`
+  // Pressure.innerHTML = `${currWeatherinput.data.pressure}hPa`
+  // //    weatherInp = " "
+  //   })
+  //   .catch((error) => {
+  //     // handle error
+  //     console.log(error);
+  //   })
+try{
+   let currWeatherinput = await axios.get(`/currentWeather/${userInput}`)
+  console.log(currWeatherinput.data);
+  if(forecastPart.classList.contains(`hidden`)){
+    forecastPart.classList.remove(`hidden`)
+    forecastPart.classList.add(`flex`)
+  }else{
+   forecastContainer.classList.add(`hidden`)
+   getWeatherForecast.classList.remove(`hidden`)
+  }
+  countryNam.innerText = `${currWeatherinput.data.country} / ${userInput}`
+  $Date.innerText = currWeatherinput.data.date
+  Temp.innerText = `${currWeatherinput.data.temp}°C`
+  Humidity.innerText = `${currWeatherinput.data.humidity}%`
+  FeelsLike.innerText = `${currWeatherinput.data.feels_like}°C`
+  Pressure.innerHTML = `${currWeatherinput.data.pressure}hPa`
+}
+catch(error){
+
+let openModal = (error) => {
+  const modalText = document.getElementById(`Response`);
+  if(weatherInp.value.length == 0){
+    modalText.innerText = `Kindly enter the valid City Name`
+    console.log(modalText.innerText)
+  }
+  else{
+    modalText.innerText = error.response.data
+  }
+    console.log(error.response.data)
+    modal.classList.remove('hidden');
+  setTimeout( () => {
+     modal.classList.add('modal-open');
+     modalContainer.classList.add('modal-container-open');
+  }, 50);
+}
+openModal(error)
+closeModal.addEventListener('click',  () => {
+  event.preventDefault()
+    modal.classList.remove('modal-open');
+    modalContainer.classList.remove('modal-container-open');
+  setTimeout( () => {
+    modal.classList.add('hidden');
+  }, 300);
+});
+modalOverlay.addEventListener('click', () => {
+  event.preventDefault()
+    modal.classList.remove('modal-open');
+    modalContainer.classList.remove('modal-container-open');
+  setTimeout( () => {
+    modal.classList.add('hidden');
+  }, 300);
+});
+}  
+})
+getWeatherForecast.addEventListener(`click`, async () => {
+
+forecastContainer.innerHTML = ' '
+forecastContainer.classList.remove(`hidden`)
+forecastContainer.classList.add(`flex`,`flex-col`)  
+{/* <div class="flex flex-col  bg-blue-600/40 space-y-2 rounded-2xl p-2 border-2 border-blue-800 ">
+        <div class="flex justify-around text-2xl w-full text-center text-white font-semibold border-t-2 border-blue-600 border-solid border-b-2">
+            <span id="Date">Date</span>
+        </div>
+        <div class="flex  justify-around items-center">
+            <img src="./images/CloudLOGO-removebg-preview.png" class="w-44 max-h-36 " alt="404 not-found">
+            <p class="text-3xl text-center text-white" id="Temp">--°C</p>
+        </div>
+        <div class="flex justify-around p-1 bg-blue-600 rounded-2xl">
+            <label for="" class="font-semibold  text-white  text-lg">Humidity</label>
+            <p class="text-lg text-center text-white" id="Humidity">N / A</p>
+        </div>
+        <div class="flex justify-around p-1 bg-blue-600 rounded-2xl">
+            <label for="" class="font-semibold  text-white  text-lg">FeelsLike</label>
+            <p class="text-lg text-center text-white" id="FeelsLike">N / A</p>
+        </div>
+        <div class="flex justify-around p-1 bg-blue-600 rounded-2xl">
+            <label for="" class="font-semibold  text-white  text-lg">Pressure</label>
+            <p class="text-lg text-center text-white" id="Pressure">N / A</p>
+        </div>
+      </div> */}
+  try {
     let userInput = document.getElementById(`weatherInp`).value
 
-  axios.get(`/currentWeather/${userInput}`)
-  .then((response) => {
+    let forecastData = await axios.get(`/weatherForecast/${userInput}`)
+    let forecastDataArray = forecastData.data
     // handle success
-    console.log(response);
-
-
+  //console.log(forecastData.data);
+  forecastDataArray.forEach(forecast => {
+  console.log(forecast)
+  let forecastDataContainer = document.createElement(`div`)
+  let dateContainer = document.createElement(`div`)
+  let TempContainer = document.createElement(`div`)
+  let humidityContainer = document.createElement(`div`)
+  let feelslikeContainer = document.createElement(`div`)
+  let pressureContainer = document.createElement(`div`)
+  let dateSpanContainer = document.createElement(`span`)
+  let dateSpantxt = document.createTextNode(forecast.date)
+  let imgContainer = document.createElement(`img`)
+  let tempParaContainer = document.createElement(`p`)
+  let tempParatxt = document.createTextNode(`${forecast.temp}°C`)
+  let humidityParaContainer = document.createElement(`p`)
+  let humidityParatxt = document.createTextNode(`${forecast.humidity}%`)
+  let pressureParaContainer = document.createElement(`p`)
+  let pressureParatxt = document.createTextNode(`${forecast.pressure}hPa`)
+  let feelslikeParaContainer = document.createElement(`p`)
+  let feelslikeParatxt = document.createTextNode(`${forecast.feels_like}°C`)
+  let humidityLabelContainer = document.createElement(`label`)
+  let humiditylabeltxt = document.createTextNode(`Humidity`)
+  let feelslikeLabelContainer = document.createElement(`label`)
+  let feelslikelabeltxt = document.createTextNode(`FeelsLike`)
+  let pressureLabelContainer = document.createElement(`label`)
+  let pressurelabeltxt = document.createTextNode(`Pressure`)
+  tempParaContainer.appendChild(tempParatxt)
+  imgContainer.src = `./images/CloudLOGO-removebg-preview.png`
+  humidityParaContainer.appendChild(humidityParatxt)
+  pressureParaContainer.appendChild(pressureParatxt)
+  feelslikeParaContainer.appendChild(feelslikeParatxt)
+  humidityLabelContainer.appendChild(humiditylabeltxt)
+  feelslikeLabelContainer.appendChild(feelslikelabeltxt)
+  pressureLabelContainer.appendChild(pressurelabeltxt)
+  dateSpanContainer.appendChild(dateSpantxt)
+  dateContainer.appendChild(dateSpanContainer)
+  TempContainer.append(imgContainer,tempParaContainer)
+  humidityContainer.append(humidityLabelContainer,humidityParaContainer)
+  feelslikeContainer.append(feelslikeLabelContainer,feelslikeParaContainer)
+  pressureContainer.append(pressureLabelContainer,pressureParaContainer)
+  forecastDataContainer.append(dateContainer, TempContainer, humidityContainer, feelslikeContainer, pressureContainer)
+  console.log(forecastDataContainer)
+  forecastContainer.appendChild(forecastDataContainer)
+  forecastDataContainer.classList.add(`flex`,`flex-col`,`bg-blue-600/40`,`space-y-2`,`rounded-2xl`,`p-2`,`border-2`,`border-blue-800`)
+  dateContainer.classList.add(`flex`,`justify-around`,`text-2xl`,`w-full`,`text-center`,`text-white`,`font-semibold`,`border-t-2`,`border-blue-600`,`border-solid`, `border-b-2`)
+  TempContainer.classList.add(`flex` , `justify-around` , `items-center`)
+  tempParaContainer.classList.add(`text-3xl` ,`text-center`,`text-white`)
+  imgContainer.classList.add(`w-44`,`max-h-36`)
+  imgContainer.setAttribute(`alt`,`404 image not found`)
+  humidityContainer.classList.add(`flex`,`justify-around`, `p-1`, `bg-blue-600`, `rounded-2xl`)
+  humidityLabelContainer.classList.add(`font-semibold`, `text-white` , `text-lg`)
+  humidityParaContainer.classList.add(`text-lg`, `text-center`, `text-white`)
+  feelslikeContainer.classList.add(`flex`,`justify-around`, `p-1`, `bg-blue-600`, `rounded-2xl`)
+  feelslikeLabelContainer.classList.add(`font-semibold`, `text-white` , `text-lg`)
+  feelslikeParaContainer.classList.add(`text-lg`, `text-center`, `text-white`)
+  pressureContainer.classList.add(`flex`,`justify-around`, `p-1`, `bg-blue-600`, `rounded-2xl`)
+  pressureLabelContainer.classList.add(`font-semibold`, `text-white` , `text-lg`)
+  pressureParaContainer.classList.add(`text-lg`, `text-center`, `text-white`)
+  console.log(forecastData)
+});
     
-  })
-  .catch((error) => {
+  }
+  catch (error) {
     // handle error
     console.log(error);
-  })
-
+  }
+  getWeatherForecast.classList.add(`hidden`)
 })
-
